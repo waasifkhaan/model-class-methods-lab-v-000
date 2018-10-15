5497795acc9b8618dc03a50ac185d665f3c6ade2
Wasif Khan <waasif.khaan@gmail.com>
2018-10-15 23:10:31 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -31,10 +31,15 @@ class Boat < ActiveRecord::Base
     joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
 
   end
-  def self.catamaran_operators
 
-    all.where(classifications: {name: 'Catamaran'})
+  def self.non_sailboats
+    where("id NOT IN (?)", self.sailboats.pluck(:id))
   end
+
+  def self.longest
+    order('length DESC').first
+  end
+  
 
 
 end
