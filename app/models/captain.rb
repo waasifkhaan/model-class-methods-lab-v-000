6ccfce74132a0ed75c3c2bb6bfd2754654d07818
Wasif Khan <waasif.khaan@gmail.com>
2018-10-15 23:19:42 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -19,5 +19,8 @@ class Captain < ActiveRecord::Base
   def self.talented_seafarers
     where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+  end
 
 end
