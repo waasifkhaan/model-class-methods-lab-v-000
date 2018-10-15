43620d713f4ed587af9e392fa96c5b346426c105
Wasif Khan <waasif.khaan@gmail.com>
2018-10-15 23:05:00 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -29,10 +29,10 @@ class Boat < ActiveRecord::Base
 
   def self.with_three_classifications
     joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
-
+    
   end
   def self.catamaran_operators
-
+    all.where(classifications: {name: 'catamaran'}
   end
 
 
