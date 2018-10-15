743ab158063a6de70bb14ef6b4ae0eb25c08a24b
Wasif Khan <waasif.khaan@gmail.com>
2018-10-15 23:11:09 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,6 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    binding.pry
-    all.where(classifications: {name: 'Catamaran'})
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
   end
 end
