5ca25601502e4f515a31d8560cb3f446d9bce98a
Wasif Khan <waasif.khaan@gmail.com>
2018-10-15 23:14:15 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -6,7 +6,7 @@ class Captain < ActiveRecord::Base
   end
 
   def self.sailors
-    includes(boats: :classifications).where(classifications: {name: "Sailboat"})
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
 
   end
 end
